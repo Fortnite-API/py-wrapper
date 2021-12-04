153258319198088cbd8f41e369c91b4347df9bed
NextChai <75498301+NextChai@users.noreply.github.com>
2021-12-04 01:04:36 -0500

    Update account.py
    - Add license to top of file.
    - Add "dunder" methods to compare two Accounts.
    - Update documentation for these changes

--- fortnite_api/account.py
+++ fortnite_api/account.py
@@ -1,5 +1,50 @@
+"""
+MIT License
+
+Copyright (c) 2019 Lucas Hardt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Dict, Union
+
 class Account:
     """Represents a account.
+    
+    .. container:: operations
+
+        .. describe:: x == y
+
+            Checks if the two accounts are equal.
+        
+        .. describe:: x != y
+
+            Checks if two accounts are not equal.
+        
+        .. describe:: str(x)
+            
+            Returns the account's name.
+        
+        .. describe:: hash(x)
+
+            Returns the account's hash.
 
     Attributes
     -----------
@@ -10,9 +55,27 @@ class Account:
     raw_data: :class:`dict`
         The raw data from request. Can be used for saving and re-creating the class.
     """
+    if TYPE_CHECKING:
+        external_auths: Dict # Adding when User lookup feature is enabled
 
     def __init__(self, data):
-        self.id = data.get('id')
-        self.name = data.get('name')
-        self.external_auths = None  # Adding when User lookup feature is enabled
-        self.raw_data = data
+        self.id: str = data.get('id')
+        self.name: str = data.get('name')
+        self.raw_data: Dict[Any, Any] = data
+        
+    def __str__(self) -> str:
+        return self.name
+
+    def __hash__(self) -> int:
+        return hash((self.id, self.name))
+    
+    def __eq__(self, o: Union[object, Account]) -> bool:
+        if not isinstance(o, Account):
+            return False
+        
+        return self.id == o.id 
+    
+    def __ne__(self, o: Union[object, Account]) -> bool:
+        return not self.__eq__(o)
+    
+    
