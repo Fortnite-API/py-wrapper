daf4db06c3c3dd99e6f9fd51d0d6da813b5fea60
NextChai <75498301+NextChai@users.noreply.github.com>
2021-12-30 00:09:18 -0500

    Move aes.py to new typed system

--- fortnite_api/aes.py
+++ fortnite_api/aes.py
@@ -23,10 +23,19 @@ SOFTWARE.
 """
 from __future__ import annotations
 
-from typing import Tuple, List, Union, Dict
-
 import re
-from datetime import datetime
+from typing import TYPE_CHECKING, Optional, Tuple, List, Union
+
+from .utils import _parse_time
+
+if TYPE_CHECKING:
+    from .types.aes import (
+        Aes as AesPayload,
+        DynamicKey as DynamicKeyPayload,
+    )
+
+if TYPE_CHECKING:
+    import datetime
 
 
 class AES:
@@ -51,15 +60,15 @@ class AES:
             Returns the AES main key and build's hash.
 
     Attributes
-    -----------
+    ----------
     main_key: :class:`str`
         The main encryption key.
     build: :class:`str`
         The current build where the AES key refers to.
     version: :class:`str`
         The current version where the AES key refers to.
-    updated: :class:`datetime.datetime`
-        The date where the AES was updates.
+    updated: Optional[:class:`datetime.datetime`]
+        The date where the AES was updates, if available.
     dynamic_keys: List[:class:`DynamicKey`]
         All current dynamic keys
     raw_data: :class:`dict`
@@ -67,20 +76,22 @@ class AES:
     """
     __slots__: Tuple[str, ...] = ('main_key', 'build', 'version', 'updated', 'dynamic_keys', 'raw_data')
 
-    def __init__(self, data):
-        self.main_key: str = data.get('mainKey')
-        self.build: str = data.get('build')
-        self.version: str = re.search(r'\d\d.\d\d', self.build)[0] if self.build else None 
-        try:
-            self.updated = datetime.strptime(data.get('updated'), '%Y-%m-%dT%H:%M:%S%z')
-        except (ValueError, TypeError):
-            self.updated = None
+    def __init__(self, data: AesPayload):
+        self.main_key: str = data['mainKey']
+        self.build: str = data['build']
+        self.version: str = re.findall(r'(?P<version>[0-9]{2})\.(?P<subversion>[0-9]{2})', self.build)[0]
 
+        self.updated: Optional[datetime.datetime]
+        try:
+            self.updated = _parse_time(data['updated']) 
+        except KeyError:
+            self.updated = None
+        
         self.dynamic_keys: List[DynamicKey] = []
-        for key_data in data.get('dynamicKeys', []) if data.get('dynamicKeys') else []:
+        for key_data in data.get('dynamicKeys', []):
             self.dynamic_keys.append(DynamicKey(key_data))
             
-        self.raw_data: Dict = data
+        self.raw_data: AesPayload = data
 
     def __str__(self):
         return self.main_key
@@ -119,20 +130,23 @@ class DynamicKey:
             Returns the dynamic key's hash.
     
     Attributes
-    -----------
+    ----------
     pak_filename: :class:`str`
         The pak's filename.
     pak_guid: :class:`str`
         The pak's guid.
     key: :class:`str`
         The key.
+    raw_data: :class:`dict`
+        The raw data from request. Can be used for saving and re-creating the class.
     """
-    __slots__: Tuple[str, ...] = ('pak_filename', 'pak_guid', 'key')
+    __slots__: Tuple[str, ...] = ('pak_filename', 'pak_guid', 'key', 'raw_data')
 
-    def __init__(self, data):
-        self.pak_filename: str = data.get('pakFilename')
-        self.pak_guid: str = data.get('pakGuid')
-        self.key: str = data.get('key')
+    def __init__(self, data: DynamicKeyPayload):
+        self.pak_filename: str = data['pakFilename']
+        self.pak_guid: str = data['pakGuid']
+        self.key: str = data['key']
+        self.raw_data: DynamicKeyPayload = data
 
     def __hash__(self) -> int:
         return hash((self.pak_filename, self.pak_guid, self.key))
