a24c7eb07ea1e593421f7580ca874c0773b6b9bd
Lucas Hardt <Luc1412.lh@gmail.com>
2021-12-30 12:57:12 +0100

    Update License
    Took 10 minutes

--- fortnite_api/types/account.py
+++ fortnite_api/types/account.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2019 Lucas Hardt
+Copyright (c) 2019-present Luc1412
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from typing import Dict, TypedDict, Any
 
 
