4e973c56b67e4e6afc8e7331346a937b0404c90d
NextChai <75498301+NextChai@users.noreply.github.com>
2022-05-26 13:31:24 -0400

    Reformat with black

--- fortnite_api/aes.py
+++ fortnite_api/aes.py
@@ -34,33 +34,31 @@ if TYPE_CHECKING:
         Aes as AesPayload,
         DynamicKey as DynamicKeyPayload,
     )
-    
+
     import datetime
 
-__all__: Tuple[str, ...] = (
-    'AES',
-    'DynamicKey'
-)
+__all__: Tuple[str, ...] = ('AES', 'DynamicKey')
 
 VERSION_REGEX: re.Pattern[str] = re.compile(r'(?P<version>[0-9]{2})\.(?P<subversion>[0-9]{2})')
 
+
 class AES:
     """Represents a AES Code.
-    
+
     .. container:: operations
-    
+
         .. describe:: x == y
 
             Checks if the two AES objects are equal.
-        
+
         .. describe:: x != y
 
             Checks if two AES objects are not equal.
 
         .. describe:: str(x)
-            
+
             Returns the AES main key.
-            
+
         .. describe:: hash(x)
 
             Returns the AES main key and build's hash.
@@ -80,19 +78,20 @@ class AES:
     raw_data: :class:`dict`
         The raw data from request. Can be used for saving and re-creating the class.
     """
+
     __slots__: Tuple[str, ...] = ('main_key', 'build', 'version', 'updated', 'dynamic_keys', 'raw_data')
 
     def __init__(self, data: AesPayload):
         self.main_key: str = data['mainKey']
         self.build: str = data['build']
         self.version: str = VERSION_REGEX.findall(self.build)[0]
-        self.updated: datetime.datetime = parse_time(data['updated']) 
+        self.updated: datetime.datetime = parse_time(data['updated'])
         self.dynamic_keys: List[DynamicKey] = [DynamicKey(entry) for entry in data.get('dynamicKeys', [])]
         self.raw_data: AesPayload = data
 
     def __str__(self):
         return self.main_key
-    
+
     def __eq__(self, o: Union[object, AES]):
         if not isinstance(o, AES):
             return False
@@ -100,32 +99,32 @@ class AES:
 
     def __ne__(self, o: Union[object, AES]) -> bool:
         return not self.__eq__(o)
-    
+
     def __hash__(self) -> int:
         return hash((self.build, self.main_key))
 
 
 class DynamicKey:
     """Represents a dynamic key.
-    
+
     .. containter:: operations
-    
+
         .. describe:: x == y
 
             Checks if the two dynamic key's are equal.
-        
+
         .. describe:: x != y
 
             Checks if two dynamic key's are not equal.
-        
+
         .. describe:: str(x)
-            
+
             Returns the dynamic key's pak filename.
-        
+
         .. describe:: hash(x)
 
             Returns the dynamic key's hash.
-    
+
     Attributes
     ----------
     pak_filename: :class:`str`
@@ -137,6 +136,7 @@ class DynamicKey:
     raw_data: :class:`dict`
         The raw data from request. Can be used for saving and re-creating the class.
     """
+
     __slots__: Tuple[str, ...] = ('pak_filename', 'pak_guid', 'key', 'raw_data')
 
     def __init__(self, data: DynamicKeyPayload):
@@ -151,12 +151,11 @@ class DynamicKey:
     def __eq__(self, o: Union[object, DynamicKey]) -> bool:
         if not isinstance(o, DynamicKey):
             return False
-        
+
         return self.pak_filename == o.pak_filename and self.pak_guid == o.pak_guid and self.key == o.key
-    
+
     def __ne__(self, o: object) -> bool:
         return not self.__eq__(o)
-    
+
     def __str__(self) -> str:
         return self.pak_filename
-    
