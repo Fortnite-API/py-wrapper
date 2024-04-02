e70b2644b9db63282ecad3273a761af0f17cc22e
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-02 00:14:40 +0000

    Impl some base async tests, will add more as I go

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,37 +1,106 @@
 import pytest
 import aiohttp
-from pprint import pprint as print
+import datetime
 
-import fortnite_api
+import fortnite_api as fn_api
+from fortnite_api.types.account import Account as AccountPayload
+
+TEST_ACCOUNT_ID = "4735ce9132924caf8a5b17789b40f79c"
+TEST_ACCOUNT_NAME = "Ninja"
+TEST_CREATOR_CODE = "ninja"
+
 
 @pytest.mark.asyncio
-async def test_async_client():
-    session = aiohttp.ClientSession()
-    async with fortnite_api.AsyncFortniteAPI(session=session) as client:
+async def test_async_client_initialization():
+    async with aiohttp.ClientSession() as session, fn_api.FortniteAPI(session=session) as client:
+        assert client
 
-        cosmetics = await client.fetch_cosmetics()
-        print(cosmetics[0].id)
+    assert session.closed == True, "Session should be closed after client is closed"
 
-        shop = await client.fetch_br_shop()
-        print(shop)
+    async with fn_api.FortniteAPI() as client:
+        assert client
 
-        cosmetic = await client.fetch_cosmetic('CID_461_Athena_Commando_M_DriftSummer')
-        print(cosmetic)
+    client_session = client.http.session
+    assert client_session and client_session.closed
 
-        cosmetics = await client.search_cosmetic(multiple=True, id='CID_298_Athena_Commando_F_IceMaiden')
-        print(cosmetics)
 
+@pytest.mark.asyncio
+async def test_async_aes():
+    async with fn_api.FortniteAPI() as client:
         aes = await client.fetch_aes()
-        print(aes)
 
+        # Ensure that the AES can be fetched with BASE64
+        aes_b64 = await client.fetch_aes(key_format=fn_api.KeyFormat.BASE64)
+
+    assert isinstance(aes, fn_api.Aes)
+    assert aes.main_key
+    assert aes.build
+    assert aes.version
+
+    assert aes.updated
+    assert isinstance(aes.updated, datetime.datetime)
+
+    assert aes == aes
+    assert aes != None
+
+    # Ensure that the AES can be fetched with BASE64
+    assert aes_b64.build == aes.build
+    assert aes_b64.version == aes.version
+
+    # NOTE: Comparison functions will not account for separate key formats, if the two instances have different values they are deemed unequal. Maybe change this in the future.
+    assert aes_b64 != aes
+
+
+@pytest.mark.asyncio
+async def test_async_banners():
+    async with fn_api.FortniteAPI() as client:
         banners = await client.fetch_banners()
-        print(len(banners))
 
+    for banner in banners:
+        assert isinstance(banner, fn_api.Banner)
+
+        assert banner.id
+        assert banner.name
+        assert banner.dev_name
+        assert banner.description
+        assert banner.category
+        assert banner.full_usage_rights is not None
+        # TODO: Banner images tests (not added because of pending Image class implementation)
+
+
+@pytest.mark.asyncio
+async def test_async_banner_colors():
+    async with fn_api.FortniteAPI() as client:
         banner_colors = await client.fetch_banner_colors()
-        print(len(banner_colors))
 
-        code = await client.fetch_creator_code('Ninja')
-        print(code)
+    for color in banner_colors:
+        assert isinstance(color, fn_api.BannerColor)
 
-        fortnite_map = await client.fetch_map()
-        print(fortnite_map)
+        assert color.id
+        assert color.color
+        assert color.category
+        assert color.sub_category_group is not None
+
+    if banner_colors:
+        first = banner_colors[0]
+        assert first == first
+
+        if len(banner_colors) >= 2:
+            assert first != banner_colors[1]
+
+
+@pytest.mark.asyncio
+async def test_async_creator_code():
+
+    async with fn_api.FortniteAPI() as client:
+        creator_code = await client.fetch_creator_code(TEST_CREATOR_CODE)
+
+    assert isinstance(creator_code, fn_api.CreatorCode)
+    assert creator_code.code == TEST_CREATOR_CODE
+
+    mock_account_payload = AccountPayload(id=TEST_ACCOUNT_ID, name=TEST_ACCOUNT_NAME)
+    assert creator_code.account == fn_api.Account(mock_account_payload)
+
+    assert creator_code.status is fn_api.CreatorCodeStatus.ACTIVE
+    assert creator_code.disabled is False
+    assert creator_code.verified is False
