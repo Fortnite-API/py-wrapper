7fba1e949aaaec4fbc00ef12413607e909165865
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-05 00:39:30 +0000

    Update how token is handled

--- fortnite_api/http.py
+++ fortnite_api/http.py
@@ -76,7 +76,7 @@ class HTTPMixin(abc.ABC):
 
         self.headers: Dict[str, Any] = {'User-Agent': self.user_agent}
         if self.token is not None:
-            self.headers['Authorization'] = self.token
+            self.headers['x-api-key'] = self.token
 
     @abc.abstractmethod
     def request(self, route: Route, **kwargs: Any) -> Any: ...
