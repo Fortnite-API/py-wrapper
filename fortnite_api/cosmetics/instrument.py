bcd6ab9971263d648cad0f6170432890e8408106
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-06 18:07:27 +0000

    Add instrument cosmetic

--- fortnite_api/cosmetics/instrument.py
+++ fortnite_api/cosmetics/instrument.py
@@ -24,8 +24,38 @@ SOFTWARE.
 
 from __future__ import annotations
 
+import datetime
+from typing import Any, Dict, Optional, List
+
+from ..utils import get_with_fallback, parse_time
+
 from ..http import HTTPClientT
-from .common import Cosmetic
+from .common import Cosmetic, CosmeticImages, CosmeticRarity, CosmeticSeries, CosmeticType
 
 
-class CosmeticInstrument(Cosmetic[HTTPClientT]): ...
+class CosmeticInstrument(Cosmetic[HTTPClientT]):
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        super().__init__(data=data, http=http)
+
+        self.name: str = data['name']
+        self.description: str = data['description']
+
+        _type = data.get('type')
+        self.type: Optional[CosmeticType] = _type and CosmeticType(data=_type)
+
+        _rarity = data.get('rarity')
+        self.rarity: Optional[CosmeticRarity] = _rarity and CosmeticRarity(data=_rarity)
+
+        _images = data.get('images')
+        self.images: Optional[CosmeticImages[HTTPClientT]] = _images and CosmeticImages(data=_images, http=http)
+
+        _series = data.get('series')
+        self.series: Optional[CosmeticSeries[HTTPClientT]] = _series and CosmeticSeries(data=_series, http=self._http)
+
+        self.gameplay_tags: List[str] = get_with_fallback(data, 'gameplayTags', list)
+        self.path: Optional[str] = data.get('path')
+        self.showcase_video: Optional[str] = data.get('showcaseVideo')
+
+        self.shop_history: List[datetime.datetime] = [
+            parse_time(time) for time in get_with_fallback(data, 'shopHistory', list)
+        ]
