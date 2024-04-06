9354dd857e25a8c78cf2949842bbdc245c3af633
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-06 20:21:18 +0000

    Update for corrected license name

--- fortnite_api/utils.py
+++ fortnite_api/utils.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2019 Lucas Hardt
+Copyright (c) 2019 Luc1412
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
