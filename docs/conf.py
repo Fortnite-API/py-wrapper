144bdbcf3ebef86a803691de436929dca23f2256
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-06 20:14:15 +0000

    Change for furo default theme, for now.

--- docs/conf.py
+++ docs/conf.py
@@ -152,7 +152,7 @@ html_experimental_html5_writer = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 html_context = {}
 
