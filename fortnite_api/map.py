53e1ccfb49c2749b2dc48f346a1307461d5af0a5
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-07 15:57:21 +0000

    Refactor map and add documentation

--- fortnite_api/map.py
+++ fortnite_api/map.py
@@ -24,38 +24,110 @@ SOFTWARE.
 
 from __future__ import annotations
 
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, Generic, List, Mapping, Optional, Tuple
 
 from .abc import Hashable
+from .asset import Asset
+from .http import HTTPClientT
 
 __all__: Tuple[str, ...] = ('Map', 'POI', 'POILocation')
 
 
-class Map:
-    __slots__: Tuple[str, ...] = ('blank_image', 'poi_image', 'pois', 'raw_data')
+class MapImages(Generic[HTTPClientT]):
+    """Represents the images of a given POI map.
 
-    def __init__(self, data: Dict[Any, Any]) -> None:
-        self.blank_image: str = data['images']['blank']
-        self.poi_image: str = data['images']['pois']
-        self.pois: List[POI] = [POI(x) for x in data['pois']]
-        self.raw_data = data
+    Attributes
+    ----------
+    blank: :class:`Asset`
+        The asset pointing to an image of the map that does not contain any POI names.
+    pois: :class:`Asset`
+        The asset pointing to an image of the map that contains the POI names.
+    """
+
+    __slots__: Tuple[str, ...] = ('blank', 'pois')
+
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.blank: Asset[HTTPClientT] = Asset(http=http, url=data['blank'])
+        self.pois: Asset[HTTPClientT] = Asset(http=http, url=data['pois'])
+
+
+class Map(Generic[HTTPClientT]):
+    """Represents a Fortnite map.
+
+    Attributes
+    ----------
+    images: :class:`MapImages`
+        The images of the map.
+    raw_data: :class:`dict`
+        The raw data of the map.
+    """
+
+    __slots__: Tuple[str, ...] = ('images', '_pois', 'raw_data')
+
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.images: MapImages[HTTPClientT] = MapImages(data=data['images'], http=http)
+
+        self._pois: Mapping[str, POI] = {poi['id']: POI(data=poi) for poi in data['pois']}
+        self.raw_data: Dict[str, Any] = data
+
+    @property
+    def pois(self) -> List[POI]:
+        """List[:class:`POI`]: The list of POIs in the map."""
+        return list(self._pois.values())
+
+    def get_poi(self, /, id: str) -> Optional[POI]:
+        """Get a POI by its ID.
+
+        Parameters
+        ----------
+        id: :class:`str`
+            The ID of the POI.
+
+        Returns
+        -------
+        Optional[:class:`POI`]
+            The POI if found, else ``None``.
+        """
+        return self._pois.get(id)
 
 
 class POI(Hashable):
-    __slots__: Tuple[str, ...] = ('id', 'name', 'location', 'raw_data')
+    """Represents a specific POI in a Fortnite map.
 
-    def __init__(self, data: Dict[Any, Any]) -> None:
+    Attributes
+    ----------
+    id: :class:`str`
+        The ID of the POI.
+    name: :class:`str`
+        The name of the POI.
+    location: :class:`POILocation`
+        The location of the POI.
+    """
+
+    __slots__: Tuple[str, ...] = ('id', 'name', 'location')
+
+    def __init__(self, *, data: Dict[str, Any]) -> None:
         self.id = data['id']
         self.name = data['name']
-        self.location = POILocation(data['location'])
-        self.raw_data = data
+        self.location = POILocation(data=data['location'])
 
 
 class POILocation:
-    __slots__: Tuple[str, ...] = ('x', 'y', 'z', 'raw_data')
+    """Holds the x, y, z coordinates of a POI in a Fortnite map.
 
-    def __init__(self, data: Dict[Any, Any]) -> None:
-        self.x: int = data['x']
-        self.y: int = data['y']
-        self.z: int = data['z']
-        self.raw_data: Dict[Any, Any] = data
+    Attributes
+    ----------
+    x: :class:`float`
+        The x coordinate.
+    y: :class:`float`
+        The y coordinate.
+    z: :class:`float`
+        The z coordinate.
+    """
+
+    __slots__: Tuple[str, ...] = ('x', 'y', 'z')
+
+    def __init__(self, *, data: Dict[str, Any]) -> None:
+        self.x: float = data['x']
+        self.y: float = data['y']
+        self.z: float = data['z']
