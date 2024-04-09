f81cea09b2b9b2ca4aa6d7e888bb1e2873490b4a
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-09 23:55:42 +0000

    Implement shop objects

--- fortnite_api/shop.py
+++ fortnite_api/shop.py
@@ -29,8 +29,87 @@ from typing import Any, Dict, Generic, List, Optional
 
 from .abc import Hashable
 from .asset import Asset
+from .cosmetics import CosmeticBr, CosmeticCar, CosmeticInstrument, CosmeticLegoKit, CosmeticTrack
 from .http import HTTPClientT
-from .utils import parse_time
+from .material import MaterialInstance
+from .utils import get_with_fallback, parse_time
+
+
+class ShopEntryBundle(Generic[HTTPClientT]):
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.name: str = data['name']
+        self.info: str = data['info']
+        self.image: Asset[HTTPClientT] = Asset(url=data['image'], http=http)
+
+
+class ShopEntryBanner:
+    def __init__(self, *, data: Dict[str, Any]) -> None:
+        self.value: str = data['value']
+        self.intensity: str = data['intensity']
+        self.backend_value: str = data['backendValue']
+
+
+class ShopEntryLayout(Hashable, Generic[HTTPClientT]):
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.id: str = data['id']
+        self.name: str = data['name']
+        self.category: Optional[str] = data.get('category')
+        self.index: int = data['index']
+        self.show_ineligible_offers: str = data['showIneligibleOffers']
+
+        _background = data.get('background')
+        self.background: Optional[Asset[HTTPClientT]] = _background and Asset(url=_background, http=http)
+
+
+class ShopEntryNewDisplayAsset(Hashable, Generic[HTTPClientT]):
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.id: str = data['id']
+        self.cosmetic_id: Optional[str] = data.get('cosmeticId')
+        self.material_instances: List[MaterialInstance[HTTPClientT]] = [
+            MaterialInstance(data=instance, http=http) for instance in get_with_fallback(data, 'materialInstances', list)
+        ]
+
+
+class ShopEntry(Generic[HTTPClientT]):
+    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
+        self.regular_price: int = data['regularPrice']
+        self.final_price: int = data['finalPrice']
+
+        _bundle = data.get('bundle')
+        self.bundle: Optional[ShopEntryBundle[HTTPClientT]] = _bundle and ShopEntryBundle(data=_bundle, http=http)
+
+        _banner = data.get('banner')
+        self.banner: Optional[ShopEntryBanner] = _banner and ShopEntryBanner(data=_banner)
+
+        self.giftable: bool = data['giftable']
+        self.refundable: bool = data['refundable']
+        self.sort_priority: int = data['sortPriority']
+        self.layout_id: str = data['layoutId']
+        self.layout: ShopEntryLayout[HTTPClientT] = ShopEntryLayout(data=data['layout'], http=http)
+        self.dev_name: str = data['devName']
+        self.offer_id: str = data['offerId']
+        self.display_asset_path: Optional[str] = data.get('displayAssetPath')
+        self.tile_size: str = data['tileSize']
+        self.new_display_asset_path: str = data['newDisplayAssetPath']
+        self.new_display_asset: ShopEntryNewDisplayAsset[HTTPClientT] = ShopEntryNewDisplayAsset(
+            data=data['newDisplayAsset'], http=http
+        )
+
+        self.br_items: List[CosmeticBr[HTTPClientT]] = [
+            CosmeticBr(data=item, http=http) for item in get_with_fallback(data, 'items', list)
+        ]
+        self.tracks: List[CosmeticTrack[HTTPClientT]] = [
+            CosmeticTrack(data=item, http=http) for item in get_with_fallback(data, 'tracks', list)
+        ]
+        self.instruments: List[CosmeticInstrument[HTTPClientT]] = [
+            CosmeticInstrument(data=item, http=http) for item in get_with_fallback(data, 'instruments', list)
+        ]
+        self.cars: List[CosmeticCar[HTTPClientT]] = [
+            CosmeticCar(data=item, http=http) for item in get_with_fallback(data, 'cars', list)
+        ]
+        self.lego_kits: List[CosmeticLegoKit[HTTPClientT]] = [
+            CosmeticLegoKit(data=item, http=http) for item in get_with_fallback(data, 'legoKits', list)
+        ]
 
 
 class Shop(Generic[HTTPClientT]):
@@ -40,71 +119,7 @@ class Shop(Generic[HTTPClientT]):
 
         self.vbuck_icon: Asset[HTTPClientT] = Asset(url=data['vbuckIcon'], http=http)
 
-        _featured = data.get('featured')
-        self.featured: Optional[ShopEntry[HTTPClientT]] = _featured and ShopEntry(data=_featured, http=http)
+        _entries = get_with_fallback(data, 'entries', list)
+        self.entries: List[ShopEntry[HTTPClientT]] = [ShopEntry(data=item, http=http) for item in _entries]
 
         self.raw_data: Dict[str, Any] = data
-
-
-class ShopEntry(Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
-        self.name: str = data['name']
-        self.entries: List[ShopEntryItem[HTTPClientT]] = [ShopEntryItem(data=item, http=http) for item in data['entries']]
-
-
-class ShopEntryItem(Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
-        self.regular_price: int = data['regularPrice']
-        self.final_price: int = data['finalPrice']
-
-        _bundle = data.get('bundle')
-        self.bundle: Optional[ShopEntryItemBundle[HTTPClientT]] = _bundle and ShopEntryItemBundle(data=_bundle, http=http)
-
-        _banner = data.get('banner')
-        self.banner: Optional[ShopEntryItemBanner[HTTPClientT]] = _banner and ShopEntryItemBanner(data=_banner, http=http)
-
-        self.giftable: bool = data['giftable']
-        self.refundable: bool = data['refundable']
-        self.sort_priority: int = data['sortPriority']
-        self.categories: Any = data['categories']  # Unknown at this time.
-        self.section_id: str = data['sectionId']
-        self.section: Any = data['section']  # Unknown at this time.
-        self.layout: ShopEntryItemLayout[HTTPClientT] = ShopEntryItemLayout(data=data['layout'], http=http)
-        self.dev_name: str = data['devName']
-        self.offer_id: str = data['offerId']
-        self.display_asset_path: str = data['displayAssetPath']
-        self.tile_size: str = data['tileSize']
-        self.new_display_asset_path: str = data['newDisplayAssetPath']
-        self.newDisplayAsset: ShopEntryItemNewDisplayAsset[HTTPClientT] = ShopEntryItemNewDisplayAsset(
-            data=data['newDisplayAsset'], http=http
-        )
-
-
-class ShopEntryItemBundle(Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
-        self.name: str = data['name']
-        self.info: str = data['info']
-        self.image: Asset[HTTPClientT] = Asset(url=data['image'], http=http)
-
-
-class ShopEntryItemBanner(Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
-        self.value: str = data['value']
-        self.intensity: str = data['intensity']
-        self.backend_value: str = data['backendValue']
-
-
-class ShopEntryItemLayout(Hashable, Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None:
-        self.id: str = data['id']
-        self.name: str = data['name']
-        self.category: str = data['category']
-        self.index: int = data['index']
-        self.show_ineligible_offers: str = data['showIneligibleOffers']
-
-        _background = data.get('background')
-        self.background: Optional[Asset[HTTPClientT]] = _background and Asset(url=_background, http=http)
-
-
-class ShopEntryItemNewDisplayAsset(Generic[HTTPClientT]):
-    def __init__(self, *, data: Dict[str, Any], http: HTTPClientT) -> None: ...
