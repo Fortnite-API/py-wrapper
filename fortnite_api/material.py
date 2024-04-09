ad45fa2cac0ab700622e82deca2001cb4971f333
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-09 15:02:52 +0000

    Update material instance to not use a scaling class

--- fortnite_api/material.py
+++ fortnite_api/material.py
@@ -24,8 +24,9 @@ SOFTWARE.
 
 from __future__ import annotations
 
-import dataclasses
-from typing import Any, Dict, Generic, Optional, Tuple
+from typing import Any, Dict, Generic, Tuple
+
+from .utils import get_with_fallback
 
 from .abc import Hashable
 from .asset import Asset
@@ -74,99 +75,6 @@ class MaterialInstanceColors:
         self.fall_off_color: str = data['FallOff_Color']
 
 
-@dataclasses.dataclass(slots=True, frozen=True)
-class ScalingsInfo:
-    """This class represents some information about specific scaling attributes of a Material instance.
-
-    In specific, this class represents the gradient and spotlight of a Material instance.
-
-    Attributes
-    ----------
-    hardness: :class:`float`
-        The hardness of the scaling attribute.
-    position_x: :class:`float`
-        The position of the scaling attribute on the x-axis.
-    position_y: :class:`float`
-        The position of the scaling attribute on the y-axis.
-    size: :class:`float`
-        The size of the scaling attribute.
-    intensity: Optional[:class:`float`]
-        The intensity of the scaling attribute. This will be ``None`` if the info is about a gradient,
-        otherwise it will be a float.
-    """
-
-    hardness: float
-    position_x: float
-    position_y: float
-    size: float
-    intensity: Optional[float] = None
-
-
-class MaterialInstanceScalings:
-    """Represents some information about the scaling of a Material instance."""
-
-    def __init__(self, *, data: Dict[str, Any]) -> None:
-        self.background_balance: Optional[float] = data.get('Background-Balance')
-        self.vignette_density: Optional[float] = data.get('Vignette Density')
-        self.vignette_radius: Optional[float] = data.get('Vignette Radius')
-        self.vignette_strength: Optional[float] = data.get('Vignette-Strength')
-
-        self.density_enhanced: Optional[float] = data.get('DensityEnhanced')
-        self.radius_enhanced: Optional[float] = data.get('RadiusEnhanced')
-
-        self.offset_image_x: Optional[float] = data.get('OffsetImage_X')
-        self.offset_image_y: Optional[float] = data.get('OffsetImage_Y')
-
-        self.zoom_image_percent: Optional[float] = data.get('ZoomImage_Percent')
-        self.refraction_depth_bias: Optional[float] = data.get('RefractionDepthBias')
-
-        self.effect_bump_multiply: Optional[float] = data.get('Effect-Bump-Multiply')
-        self.effect_time_factor: Optional[float] = data.get('Effect-Time-Factor')
-
-        self.sine_offset: Optional[float] = data.get('Sine Offset')
-        self.streak_angle: Optional[float] = data.get('Streak Angle')
-        self.streak_power_angle: Optional[float] = data.get('Streak Power Angle')
-
-        # TODO: Move these into objects? streak_X objects?
-        self.streak_g_multiply_max: Optional[float] = data.get('Streak-G-Multiply-Max')
-        self.streak_g_multiply_min: Optional[float] = data.get('Streak-G-Multiply-Min')
-        self.streak_g_power_max: Optional[float] = data.get('Streak-G-Power-Max')
-        self.streak_g_power_min: Optional[float] = data.get('Streak-G-Power-Min')
-        self.streak_g_u_offset: Optional[float] = data.get('Streak-G-U-Offset')
-        self.streak_g_u_tile: Optional[float] = data.get('Streak-G-U-Tile')
-        self.streak_g_v_offset: Optional[float] = data.get('Streak-G-V-Offset')
-        self.streak_g_v_tile: Optional[float] = data.get('Streak-G-V-Tile')
-        self.streak_r_multiply_max: Optional[float] = data.get('Streak-R-Multiply-Max')
-        self.streak_r_multiply_min: Optional[float] = data.get('Streak-R-Multiply-Min')
-        self.streak_r_power_max: Optional[float] = data.get('Streak-R-Power-Max')
-        self.streak_r_power_min: Optional[float] = data.get('Streak-R-Power-Min')
-        self.streak_r_u_offset: Optional[float] = data.get('Streak-R-U-Offset')
-        self.streak_r_u_tile: Optional[float] = data.get('Streak-R-U-Tile')
-        self.streak_r_v_offset: Optional[float] = data.get('Streak-R-V-Offset')
-        self.streak_r_v_tile: Optional[float] = data.get('Streak-R-V-Tile')
-
-        # TODO: These must be made optional (add check for if they are all none then don't show)
-        self.gradient: ScalingsInfo = ScalingsInfo(
-            hardness=data['Gradient_Hardness'],
-            position_x=data['Gradient_Position_X'],
-            position_y=data['Gradient_Position_Y'],
-            size=data['Gradient_Size'],
-        )
-
-        # TODO These must be made optional (add check for if they are all none then don't show)
-        self.spotlight: ScalingsInfo = ScalingsInfo(
-            hardness=data['Spotlight_Hardness'],
-            intensity=data['Spotlight_Intensity'],
-            position_x=data['Spotlight_Position_X'],
-            position_y=data['Spotlight_Position_Y'],
-            size=data['Spotlight_Size'],
-        )
-
-        self.falloff_color_fill_percent: Optional[float] = data.get('FallOffColor_Fill_Percent')
-        self.falloff_color_position: Optional[float] = data.get('FallOffColor_Postion')
-        self.offset_image_y_compensation: Optional[float] = data.get('OffsetImage_Y_Compensation')
-
-
 class MaterialInstance(Hashable, Generic[HTTPClientT]):
     """Material instancing is a way to create a parent Material that you can
     use as a base to make a wide variety of different looking children (Material instances).
@@ -187,8 +95,10 @@ class MaterialInstance(Hashable, Generic[HTTPClientT]):
         Represents some images of the Material instance, as they are rendered in game.
     colors: :class:`MaterialInstanceColors`
         Holds some metadata about the colors of the Material instance.
-    scalings: Optional[:class:`MaterialInstanceScalings`]
-        Some information about the scalings of the Material instance, if any.
+    scalings: :class:`dict`
+        Some information about the scalings of the Material instance. This is a low level Unreal Engine 5
+        information dump that is not commonly used, but is available for those who need it. It can
+        contain information about the material background, density, zoom, streak multipliers, and spotlights.
     flags: None
         The flags of the material instance.
 
@@ -205,8 +115,6 @@ class MaterialInstance(Hashable, Generic[HTTPClientT]):
 
         self.images: MaterialInstanceImages[HTTPClientT] = MaterialInstanceImages(data=data['images'], http=http)
         self.colors: MaterialInstanceColors = MaterialInstanceColors(data=data['colors'])
-
-        _scalings = data.get('scalings')
-        self.scalings: Optional[MaterialInstanceScalings] = _scalings and MaterialInstanceScalings(data=_scalings)
+        self.scalings: Dict[str, Any] = get_with_fallback(data, 'scalings', dict)
 
         self.flags: None = data['flags']  # This is always None at this time.
