2996d94af77cf17290141b141b06bbcd17980433
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-05-05 02:47:10 +0000

    Cleanup AES documentation

--- fortnite_api/aes.py
+++ fortnite_api/aes.py
@@ -73,10 +73,6 @@ class Aes:
 
             Checks if two Aes objects are not equal.
 
-        .. describe:: str(x)
-
-            Returns the Aes main key.
-
         .. describe:: hash(x)
 
             Returns the Aes main key and build's hash.
@@ -92,7 +88,7 @@ class Aes:
     build: :class:`str`
         The current build where the Aes key refers to.
     version: Optional[:class:`fortnite_api.Version`]
-        The current version where the Aes key refers to. This will only be ``None`` in the case that the API returned an invalid :attr`build` value.
+        The current version where the Aes key refers to. This will only be ``None`` in the case that the API returned an invalid :attr:`build` value.
     updated: :class:`datetime.datetime`
         The date where the Aes was updates.
     dynamic_keys: List[:class:`fortnite_api.DynamicKey`]
@@ -174,17 +170,14 @@ class DynamicKey:
         The pak's guid.
     key: :class:`str`
         The key.
-    raw_data: :class:`dict`
-        The raw data from request. Can be used for saving and re-creating the class.
     """
 
-    __slots__: Tuple[str, ...] = ('pak_filename', 'pak_guid', 'key', 'raw_data')
+    __slots__: Tuple[str, ...] = ('pak_filename', 'pak_guid', 'key')
 
     def __init__(self, data: Dict[str, Any]):
         self.pak_filename: str = data['pakFilename']
         self.pak_guid: str = data['pakGuid']
         self.key: str = data['key']
-        self.raw_data: Dict[str, Any] = data
 
     def __hash__(self) -> int:
         return hash((self.pak_filename, self.pak_guid, self.key))
