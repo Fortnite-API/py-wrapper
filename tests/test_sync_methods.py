7ddd4a00aa8a227ed4b15474f36a70a33774d2bc
Lucas Hardt <lucas.hardt@fu-berlin.de>
2024-09-03 12:21:59 +0200

    Remove assertion of color2

--- tests/test_sync_methods.py
+++ tests/test_sync_methods.py
@@ -382,7 +382,6 @@ def test_sync_fetch_shop(api_key: str):
         if colors:
             assert isinstance(colors, fn_api.ShopEntryColors)
             assert isinstance(colors.color1, str)
-            assert colors.color2
             assert isinstance(colors.color3, str)
             assert isinstance(colors.text_background_color, str)
 
