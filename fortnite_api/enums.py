74a2f036ffb321ab58515b9d7563203b15dc9489
Lucas Hardt <Luc1412.lh@gmail.com>
2024-12-17 00:30:38 +0100

    Add missing docstring for CustomGender.UNKNOWN Update changelog entry for VariantBean change

--- fortnite_api/enums.py
+++ fortnite_api/enums.py
@@ -539,6 +539,8 @@ class CustomGender(Enum):
         A female character.
     MALE
         A male character.
+    UNKNOWN
+        The character's gender is unknown.
     """
 
     FEMALE = 'EFortCustomGender::Female'
